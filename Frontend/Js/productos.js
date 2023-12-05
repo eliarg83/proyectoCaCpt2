febdc4ed2f485e00ac7b92adf0549bc281cbb3e8
eliarg83 <eliarg83@hotmail.com>
2023-12-05 20:10:04 -0300

    ultimo comit en proyecto CaCpt2

--- Frontend/Js/productos.js
+++ Frontend/Js/productos.js
@@ -3,8 +3,8 @@ const { createApp } = Vue
         data() {
             return {
                 productos: [],
-                url:'http://127.0.0.1:5000/productos', // si el backend esta corriendo local usar localhost 5000
-                // url:'http://mcerda.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+                //url:'http://127.0.0.1:5000/productos', // si el backend esta corriendo local usar localhost 5000
+                 url:'https://eliarg.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
                 error: false,
                 cargando: true,
                 /*atributos para el guardar los valores del formulario */
